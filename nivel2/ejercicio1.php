aedd129090ae2b4c91aa04b7781341a82ea83f2c
Jorge Torres <jorge.tm.ink@outlook.com>
2024-04-29 18:56:23 +0200

    Update ejercicio1.php

--- nivel2/ejercicio1.php
+++ nivel2/ejercicio1.php
@@ -1,7 +1,14 @@
 <?php
 /*
+Crea un programa que contenga dos arrays de números enteros/floados. Una vez creados, muestra por pantalla el resultado de:
 
+La intersección de los dos arrays (números en común)
+La mezcla de ambos arrays.
 */
 
+$numerosNaturales = [1, 2, 3, 4, 5];
+$numerosFlotantes = [2.3, 3.4, 5.6, 7.8, 9.1];
 
-?>
+$arrayConbinado = array_merge($numerosNaturales, $numerosFlotantes);
+
+var_dump($arrayConbinado);
